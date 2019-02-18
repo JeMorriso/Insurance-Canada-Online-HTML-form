30c096988ebf33e99333ac5d05dea7926c008bb9
JeMorriso <jeremy.morrison36@gmail.com>
2019-02-18 13:15:20 -0800

    Finish show/hide logic from section to section.

--- public/test.js
+++ public/test.js
@@ -1,32 +1,73 @@
+var smoker;
+var otherInsurance;
 
-var testListener = document.getElementsByTagName("h1")[0];
+// when user fills out smoking status, grab the value to use for control flow
+$("#smoker").on("change", function() {
+  smoker = ($("input[name='smoker']:checked").val() == 'yes');
+});
 
-testListener.addEventListener("click", function() {
-  console.log("herp derp sermberdy clercked teh buttonzz");
-})
+$("#otherInsurance").on("change", function() {
+  otherInsurance = ($("input[name='otherInsurance']:checked").val() == 'yes');
+});
 
-console.log("hello world of insurance");
-console.log("goodbye innocent world");
+// put event listener on each section so that if yes is ever selected we know not to go to the next section
+$("#section-two").on("change", function() {
+  otherInsurance = ($("input[name='otherInsurance']:checked").val() == 'yes');
+});
 
-var showHide = document.getElementsByClassName("showNext");
+$(".section-submit").on("click", function() {
+  var section = $(this).parent();
+  // hide the section
+  section.hide();
+  //figure out if next section should be shown
+  showNextSection(section)
+});
 
-for (var i=0; i < showHide.length; i++) {
-  showHide[i].addEventListener("change", function() {
-    //alert("holy TOOT! the value changed!");
-    console.log(this.value);
-    //if (showHide[i].va)
-  })
+function showNextSection(currSection) {
+  // get array of sections
+  var sectionArray = $(".section");
+  var dex = sectionArray.index(currSection);
+  var nextSection = sectionArray.eq(dex+1);
+
+  // if any questions were answered yes, then don't show the next section
+  // select all checked radion buttons in current section and iterate
+  var shouldShow = true;
+
+  // always show section 2
+  if (dex != 0) {
+    $('#'+currSection.attr('id') + " input:radio:checked").each(function() {
+
+      if ($(this).val() == 'yes') {
+        shouldShow = false;
+      }
+    });
+  }
+
+  if (shouldShow) {
+    nextSection.toggle();
+  } 
+  // show submit button if no more questions to answer
+  else {
+    sectionArray.eq(sectionArray.length-1).toggle();
+  }
 }
 
-var sectionArray = $(".section");
-sectionArray.on("change", function() {
-  console.log("holy TOOT! the value changed!");
-  // get the index of this section
-  var dex = sectionArray.index(this);
-  // show the next section
-  sectionArray.eq(dex+1).toggle();
-  console.log(sectionArray);
-  console.log(this);
-  console.log(dex);
-  
-})
+// function completedSectionAlert() {
+//   if (smoker && otherInsurance) {
+
+//   }
+// };
+
+// there is a section-submit for each section
+// var sectionArray = $(".section-submit");
+// sectionArray.on("click", function() {
+//   // get the index of this section
+//   var dex = sectionArray.index(this);
+//   // hide this section and show the next section
+//   $
+//   sectionArray.eq(dex).toggle();
+//   sectionArray.eq(dex+1).toggle();
+//   console.log(sectionArray);
+//   console.log(this);
+//   console.log(dex);
+// })
